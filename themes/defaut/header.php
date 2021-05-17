f7d6da746a4e328132fb1bab95444f192a6424a6
bazooka07 <jpourrez@gmail.com>
2021-05-17 23:51:11 +0200

    Fix themes/defaut

--- themes/defaut/header.php
+++ themes/defaut/header.php
@@ -42,18 +42,14 @@
 
 				<nav class="nav col med-6 lrg-8">
 
-					<nav class="nav">
-
-						<div class="responsive-menu">
-							<label for="menu"></label>
-							<input type="checkbox" id="menu">
-							<ul class="menu">
-								<?php $plxShow->staticList($plxShow->getLang('HOME'), '<li class="#static_class #static_status" id="#static_id"><a href="#static_url" title="#static_name">#static_name</a></li>'); ?>
-								<?php $plxShow->pageBlog('<li class="#page_class #page_status" id="#page_id"><a href="#page_url" title="#page_name">#page_name</a></li>'); ?>
-							</ul>
-						</div>
-
-					</nav>
+					<div class="responsive-menu">
+						<label for="menu"></label>
+						<input type="checkbox" id="menu">
+						<ul class="menu">
+							<?php $plxShow->staticList($plxShow->getLang('HOME'), '<li class="#static_class #static_status" id="#static_id"><a href="#static_url" title="#static_name">#static_name</a></li>'); ?>
+							<?php $plxShow->pageBlog('<li class="#page_class #page_status" id="#page_id"><a href="#page_url" title="#page_name">#page_name</a></li>'); ?>
+						</ul>
+					</div>
 
 				</nav>
 
