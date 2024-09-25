e884242f8d4d83b716da4675db66b7761f621b16
Jean-Pierre Pourrez <kazimentou@free.fr>
2024-09-25 23:56:13 +0200

    New printSelectDir() in parametres_edittpl.php

--- core/admin/parametres_edittpl.php
+++ core/admin/parametres_edittpl.php
@@ -2,12 +2,13 @@
 /**
  * Edition des fichiers templates du thème en vigueur
  * @package PLX
- * @author	Stephane F
+ * @author	Stephane F, J.P. Pourrez @bazooka07
  **/
 
 include 'prepend.php';
 
-const TEMPLATE_EXTS = array('.php', '.css', '.htm', '.html', '.txt', '.js', '.xml',);
+# extensions autorisées pour les templates
+const TEMPLATE_EXTS = array('php', 'css', 'htm', 'html', 'txt', 'js', 'xml',);
 
 # Controle du token du formulaire
 plxToken::validateFormToken($_POST);
@@ -16,8 +17,18 @@ plxToken::validateFormToken($_POST);
 $plxAdmin->checkProfil(PROFIL_ADMIN);
 
 # On récupère les fichiers templates du thèmes
-function listFolderFiles($dir, $root='') {
-	$content = array();
+function listFolderFiles($dir, $firstLevel = false) {
+	static $content = null;
+	static $root = false;
+	static $offset = 0;
+
+	if($firstLevel) {
+		# First step
+		$content= array();
+		$root = $dir;
+		$offset = strlen($dir);
+	}
+
 	$ffs = scandir($dir);
 	foreach($ffs as $ff){
 		# On ignore les fichiers/dossiers cachés et les "dossiers" . et ..
@@ -25,24 +36,73 @@ function listFolderFiles($dir, $root='') {
 			continue;
 		}
 
-		$filename = $dir . '/' . $ff;
+		$filename = $dir . $ff;
 		if(!is_dir($filename)) {
-			$ext = strtolower(strrchr($ff, '.'));
+			$ext = strtolower(pathinfo($ff, PATHINFO_EXTENSION));
 			if(in_array($ext, TEMPLATE_EXTS)) {
-				$f = str_replace($root, '', PLX_ROOT.ltrim($filename, './'));
-				$content[$f] = $f;
+				$content[] = substr($filename, $offset);
 			}
 		} else {
 			# appel récursif de la fonction
-			$content = array_merge($content, listFolderFiles($filename, $root));
+			listFolderFiles($filename . '/');
 		}
 	}
-	return $content;
+
+	if($firstLevel) {
+		return $content;
+	}
+}
+
+function printSelectDir($name, $values, $selected, $readonly=false, $class='', $id=true) {
+?>
+<select id="id_<?= $name ?>" name="<?= $name ?>" class="<?= $class ?>">
+<?php
+	$suffixe = array(
+		false => '├ ',
+		true  => '└ ',
+	);
+	$ccPath = '.';
+	$level = 0;
+	$spacer = '';
+	$lastItem = 0;
+	$cnt = array();
+	$i = 0;
+	foreach($values as $f) {
+		$path = pathinfo($f,  PATHINFO_DIRNAME);
+		if($ccPath != $path) {
+			$i = 0;
+			$level = count(explode('/', $f)) - 1;
+			if($level > 0) {
+				# On compte le nombre d'éléments dans ce dossier
+				$cnt[$level] = count(array_filter($values, function($filename) use($path) {
+					return preg_match('#^' . $path . '\b#', $filename);
+				}));
+				# gère le cas où le groupe précèdent n'a qu'un éléement
+				$spacerGroup = ($level > 1) ? str_repeat(' ', ($level -1) * 4) . '├ ' : ''; # espaces insécables !
+?>
+	<optgroup label="<?= $spacerGroup . basename($path) ?>" data-level="<?= $level -1 ?>" data-count="<?= $cnt[$level] ?>"></optgroup>
+<?php
+				$spacer = str_repeat(' ', $level * 4); # espaces insécables !
+			} else {
+				$spacer = '';
+			}
+			$ccPath = $path;
+		}
+
+		$i++;
+		$suff = ($level > 0) ? $suffixe[$cnt[$level] == $i] : '';
+?>
+	<option value="<?= $f ?>" <?= ($f == $selected) ? 'selected' : '' ?> data-level="<?= $level ?>"><?= $spacer . $suff . basename($f) ?></option>
+<?php
+	}
+?>
+</select>
+<?php
 }
 
 $style = $plxAdmin->aConf['style'];
-$root = PLX_ROOT . $plxAdmin->aConf['racine_themes'] . $style;
-$aTemplates = listFolderFiles($root, $root);
+$root = PLX_ROOT . $plxAdmin->aConf['racine_themes'] . $style . '/';
+$aTemplates = listFolderFiles($root, true);
 
 # Initialisation
 if(isset($_POST['load'])) {
@@ -53,13 +113,13 @@ if(isset($_POST['load'])) {
 	$tpl = 'home.php';
 }
 
-if(!array_key_exists('/' . $tpl, $aTemplates)) {
+if(!in_array($tpl, $aTemplates)) {
 	plxMsg::Error(L_CONFIG_EDITTPL_ERROR_NOTHEME);
-	header('Location: parametres_affichage.php');
+	header('Location: parametres_themes.php');
 	exit;
 }
 
-$filename = realpath($root . '/' . $tpl);
+$filename = realpath($root . $tpl);
 
 # Traitement du formulaire: sauvegarde du template
 if(isset($_POST['save']) AND !empty(trim($_POST['content']))) {
@@ -81,7 +141,7 @@ include 'top.php';
 		<h2><?= L_CONFIG_EDITTPL_TITLE ?> &laquo;<?= plxUtils::strCheck($style) ?>&raquo;</h2>
 		<p><?php printf(L_CONFIG_VIEW_PLUXML_RESSOURCES, PLX_RESSOURCES_THEMES_LINK); ?></p>
 		<?= plxToken::getTokenPostMethod() ?>
-		<?php plxUtils::printSelectDir('template', $tpl, $root, 'no-margin', false) ?>
+		<?php printSelectDir('template', $aTemplates, $tpl); ?>
 		<input name="load" type="submit" value="<?= L_CONFIG_EDITTPL_LOAD ?>" />
 		<span class="sml-hide med-show">&nbsp;&nbsp;&nbsp;</span>
 		<input name="save" type="submit" value="<?= L_SAVE_FILE ?>" />
